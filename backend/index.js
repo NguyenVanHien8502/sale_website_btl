fe8ce14e899e65ed5581e45a708219f991a8add4
Nguyễn Văn Hiển <hiennv@mirabo-global.com>
2024-06-22 17:32:54 +0700

    add order

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,9 @@ const prodcategoryRouter = require("./routes/prodcategoryRoute");
 const blogcategoryRouter = require("./routes/blogCatRoute");
 const brandRouter = require("./routes/brandRoute");
 const couponRouter = require("./routes/couponRoute");
+const orderAdminRouter = require("./routes/orderAdminRoute");
 const bodyParser = require("body-parser");
-const { notFound, errorHandler } = require("./middlewares/errorHandler");
+const {notFound, errorHandler} = require("./middlewares/errorHandler");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -20,7 +21,7 @@ dbConnect();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use(
@@ -39,6 +40,7 @@ app.use("/api/prodcategory", prodcategoryRouter);
 app.use("/api/blogcategory", blogcategoryRouter);
 app.use("/api/brand", brandRouter);
 app.use("/api/coupon", couponRouter);
+app.use("/api/orderAdmin", orderAdminRouter);
 
 app.use(notFound);
 app.use(errorHandler);
